5f57bf01530ccc3e029186463c14df493fefca6b
vgarciac82 <104453051+vgarciac82@users.noreply.github.com>
2022-10-16 19:55:52 -0500

    Cambios al iniciar aplicacion

--- src/main/java/com/ventas/ventas/VentasApplication.java
+++ src/main/java/com/ventas/ventas/VentasApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VentasApplication {
 
+	
 	public static void main(String[] args) {
+		System.out.println("Iniciando APP");
 		SpringApplication.run(VentasApplication.class, args);
 	}
 
